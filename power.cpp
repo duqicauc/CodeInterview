e138532506c0733c3a41d0b02a25d6029e9e2a1e
duqi <duqi_2009@163.com>
2013-12-23 17:26:08 +0800

    观察库函数对各种特殊情况（底数和指数分别为正数/零/负数）的处理结果，推测其处理方式

---
+++ power.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    cout << pow(2.5, 2) <<endl;
+    cout << pow(2.5, 0) <<endl;
+    cout << pow(2.5, -2) <<endl;
+    cout << pow(0, 2) << endl;
+    cout << pow(0, 0) << endl;
+    cout << pow(0, -2) <<endl;
+    cout << pow(-2.5, 2) <<endl;
+    cout << pow(-2.5, 0) <<endl;
+    cout << pow(-2.5, -2) <<endl;
+    return 0;
+}

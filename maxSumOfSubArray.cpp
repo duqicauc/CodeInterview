91230068503e7a55d367f20382b56a4f6172b395
duqi <duqi_2009@163.com>
2014-05-13 16:49:49 +0800

    对findMaxSumOfSubArray.cpp进行一些简单注释

--- maxSumOfSubArray.cpp
+++ maxSumOfSubArray.cpp
@@ -1,7 +1,8 @@
 /**
 求子数组的最大和：
-
-
+动态规划的思想，每进行一步，都会做出相应的决策，并进行一定的调整，在这里
+（1）决定是否将array[i]加到当前所在的子数组中的和中；
+（2）决定当前的子数组和是否为最大
 */
 #include <iostream>
 #include <assert.h>
@@ -21,12 +22,13 @@ int main(int argc, char const *argv[])
 int findMaxSumOfSubArray(int *array, int num)
 {
 	assert(array != NULL && num >0);\
-	
+
 	int max = array[0];
 	for (int i = 1; i < num; ++i)
 	{
 		// use array[i] to locate the current sum, decide if the max has the truly array[i]
 		array[i] = max(array[i],array[i-1]+array[i]); 
+		// decide if the sub is the greatest
 		if (max < array[i])
 		{
 			max = array[i];

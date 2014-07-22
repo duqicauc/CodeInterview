3407da5614cea30f2d95f390a8a3b21d5a99fd60
杜琪 <duqi_2009@163.com>
2014-07-22 12:18:17 +0800

    修正了正确的partition算法以及取随机数的方法
    （1）从右往左，找第一个小于等于key的位置r
    （2）从左往右，找第一个大于等于key的位置l
    （3）如果l<r，则交换位置

--- sort/QuickSort.cpp
+++ sort/QuickSort.cpp
@@ -3,22 +3,26 @@
 using namespace std;
 
 /*划分函数，从左和右同时扫描，并使用随机数选取划分关键字*/
-int Partition(int L[],int first, int last)
+int partition(int a[],int first, int last)
 {
 	int l=first,r=last;
-
 	srand((unsigned)time(0));
-	int key = L[rand()%r + l];
-	
+	int key = a[(rand() % (r-l+1))+ l];  //(1)去[l,r]之间的随机数
+	//int key = a[(l+r)/2];  //(2)取中间值为分割点
+
 	while(l < r){
-		while((r>l) && L[r]>=key) r--;
-		if(l<r)
-			L[l++] = L[r];
-		while((l<r) && L[l]<key) l++;
-		if(l<r)
-			L[r--] = L[l];
+		// 从右往左，找到第一个小于或者等于key的值，应该放到左边去
+		while(a[r]>key) r--;
+		// 从左往右，找到第一个大于或者等于key的值，应该放到右边去
+		while(a[l]<key) l++;
+
+		if(l < r){
+			int temp = a[l];
+			a[l] = a[r];
+			a[r] = temp;
+		}
 	}
-	L[l]=key;
+	a[l] = key; //关键字放在正确的位置上
 	return l;
 }
 
@@ -33,8 +37,14 @@ void QuickSort(int L[],int first,int last)
 int main()
 {
 	int L[]={42,19,71,49,76,90,29,88,51};
-	QuickSort(L,0,8);
 	for(int i=0; i< 9; i++)
-		cout << L[i] << endl;
+		cout << L[i] << " ";
+	cout <<endl;
+	
+	QuickSort(L,0,8);
+	
+	for(int i=0; i< 9; i++)
+		cout << L[i] << " ";
+	cout <<endl;
 	return 0;
 }
